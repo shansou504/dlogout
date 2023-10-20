5af9319427206059026d76bef02f50d723a617da
shansou504 <email@example.com>
2023-10-20 10:34:21 -0400

    rewrite

--- dlogout.c
+++ dlogout.c
@@ -1,16 +1,38 @@
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-static void
-reboot (void) {
-	system("systemctl reboot");
+static void check_escape(GtkWidget *widget, GdkEventKey *event, gpointer data)
+{
+  if (event->keyval == GDK_KEY_Escape) {
+		system("pkill -f dlogout &");
+  }
 }
 
 static void
-shutdown (void) {
-	system("systemctl poweroff");
+action (GtkListBox *listbox) {
+	GtkListBoxRow *listboxrow = gtk_list_box_get_selected_row(listbox);
+	switch (gtk_list_box_row_get_index(listboxrow)) {
+		case 0:
+			system("slock &");
+			system("xset dpms force off &");
+			system("pkill -f dlogout &");
+			break;
+		case 1:
+			system("pkill -f dwm &");
+			system("pkill -f dlogout &");
+			break;
+		case 2:
+			system("systemctl reboot");
+			break;
+		case 3:
+			system("systemctl poweroff");
+			break;
+		default:
+			system("pkill -f dlogout &");
+	} 
 }
 
 static void
@@ -20,22 +42,42 @@ activate (GtkApplication *app,
   GtkWidget *window;
   window = gtk_application_window_new (app);
 	gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
-	gtk_window_set_default_size (GTK_WINDOW (window), 80, 60);
+	gtk_window_set_default_size (GTK_WINDOW (window), 80, 80);
 	gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
 	gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_MOUSE);
+	g_signal_connect(window, "key_press_event", G_CALLBACK(check_escape), NULL);
 
-  GtkWidget *button_box;
-  button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
+  GtkWidget *listbox;
+  listbox = gtk_list_box_new ();
+  gtk_container_add (GTK_CONTAINER (window), listbox);
+	gtk_list_box_set_selection_mode (GTK_LIST_BOX(listbox), GTK_SELECTION_SINGLE);
 
-  GtkWidget *rebootbtn = gtk_button_new_with_label ("Reboot");
-  g_signal_connect (rebootbtn, "clicked", G_CALLBACK (reboot), NULL);
-  gtk_container_add (GTK_CONTAINER (button_box), rebootbtn);
+	GtkWidget *rowlock = gtk_list_box_row_new();
+	GtkWidget *labellock = gtk_label_new(" Lock");
+	gtk_container_add (GTK_CONTAINER (rowlock), labellock);
+	gtk_container_add (GTK_CONTAINER (listbox), rowlock);
+	gtk_label_set_xalign (GTK_LABEL(labellock), 0);
 
-  GtkWidget *shutdownbtn = gtk_button_new_with_label ("Shutdown");
-  g_signal_connect (shutdownbtn, "clicked", G_CALLBACK (shutdown), NULL);
-  gtk_container_add (GTK_CONTAINER (button_box), shutdownbtn);
+	GtkWidget *rowquit = gtk_list_box_row_new();
+	GtkWidget *labelquit = gtk_label_new(" Quit");
+	gtk_container_add (GTK_CONTAINER (rowquit), labelquit);
+	gtk_container_add (GTK_CONTAINER (listbox), rowquit);
+	gtk_label_set_xalign (GTK_LABEL(labelquit), 0);
+
+	GtkWidget *rowreboot = gtk_list_box_row_new();
+	gtk_container_add (GTK_CONTAINER (listbox), rowreboot);
+	GtkWidget *labelreboot = gtk_label_new(" Reboot");
+	gtk_container_add (GTK_CONTAINER (rowreboot), labelreboot);
+	gtk_label_set_xalign (GTK_LABEL(labelreboot), 0);
+
+	GtkWidget *rowshutdown = gtk_list_box_row_new();
+	GtkWidget *labelshutdown = gtk_label_new(" Shutdown");
+	gtk_container_add (GTK_CONTAINER (rowshutdown), labelshutdown);
+	gtk_container_add (GTK_CONTAINER (listbox), rowshutdown);
+	gtk_label_set_xalign (GTK_LABEL(labelshutdown), 0);
+
+	g_signal_connect (listbox, "row-activated", G_CALLBACK (action), listbox);
 
   gtk_widget_show_all (window);
 
