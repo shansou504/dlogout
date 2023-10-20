05b391c291c1058c39a1ae5194f0fac407f9146c
shansou504 <email@example.com>
2023-10-20 11:55:05 -0400

    standardized spacing in code

--- dlogout.c
+++ dlogout.c
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void check_escape(GtkWidget *widget, GdkEventKey *event, gpointer data)
+static void
+check_escape(GtkWidget *widget,
+	GdkEventKey *event,
+	gpointer data)
 {
   if (event->keyval == GDK_KEY_Escape) {
 		system("pkill -f dlogout &");
@@ -12,7 +15,8 @@ static void check_escape(GtkWidget *widget, GdkEventKey *event, gpointer data)
 }
 
 static void
-action (GtkListBox *listbox) {
+action (GtkListBox *listbox)
+{
 	GtkListBoxRow *listboxrow = gtk_list_box_get_selected_row(listbox);
 	switch (gtk_list_box_row_get_index(listboxrow)) {
 		case 0:
@@ -37,56 +41,56 @@ action (GtkListBox *listbox) {
 
 static void
 activate (GtkApplication *app,
-          gpointer        user_data)
+	gpointer        user_data)
 {
-  GtkWidget *window;
-  window = gtk_application_window_new (app);
+	GtkWidget *window;
+	window = gtk_application_window_new (app);
 	gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
 	gtk_window_set_default_size (GTK_WINDOW (window), 80, 80);
 	gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
 	gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_MOUSE);
 	g_signal_connect(window, "key_press_event", G_CALLBACK(check_escape), NULL);
-
-  GtkWidget *listbox;
-  listbox = gtk_list_box_new ();
-  gtk_container_add (GTK_CONTAINER (window), listbox);
+	
+	GtkWidget *listbox;
+	listbox = gtk_list_box_new ();
+	gtk_container_add (GTK_CONTAINER (window), listbox);
 	gtk_list_box_set_selection_mode (GTK_LIST_BOX(listbox), GTK_SELECTION_SINGLE);
-
+	
 	GtkWidget *rowlock = gtk_list_box_row_new();
 	GtkWidget *labellock = gtk_label_new("	Lock");
 	gtk_container_add (GTK_CONTAINER (rowlock), labellock);
 	gtk_container_add (GTK_CONTAINER (listbox), rowlock);
 	gtk_label_set_xalign (GTK_LABEL(labellock), 0);
-
+	
 	GtkWidget *rowquit = gtk_list_box_row_new();
 	GtkWidget *labelquit = gtk_label_new("	Quit");
 	gtk_container_add (GTK_CONTAINER (rowquit), labelquit);
 	gtk_container_add (GTK_CONTAINER (listbox), rowquit);
 	gtk_label_set_xalign (GTK_LABEL(labelquit), 0);
-
+	
 	GtkWidget *rowreboot = gtk_list_box_row_new();
 	gtk_container_add (GTK_CONTAINER (listbox), rowreboot);
 	GtkWidget *labelreboot = gtk_label_new("	Reboot");
 	gtk_container_add (GTK_CONTAINER (rowreboot), labelreboot);
 	gtk_label_set_xalign (GTK_LABEL(labelreboot), 0);
-
+	
 	GtkWidget *rowshutdown = gtk_list_box_row_new();
 	GtkWidget *labelshutdown = gtk_label_new("	hutdown");
 	gtk_container_add (GTK_CONTAINER (rowshutdown), labelshutdown);
 	gtk_container_add (GTK_CONTAINER (listbox), rowshutdown);
 	gtk_label_set_xalign (GTK_LABEL(labelshutdown), 0);
-
+	
 	g_signal_connect (listbox, "row-activated", G_CALLBACK (action), listbox);
-
-  gtk_widget_show_all (window);
-
+	
+	gtk_widget_show_all (window);
+	
 	g_signal_connect (app, "activate", G_CALLBACK (g_application_quit), app);
 }
 
 int
 main (int    argc,
-      char **argv)
+	char **argv)
 {
   GtkApplication *app;
   int status;
