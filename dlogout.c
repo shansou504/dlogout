fbea02cc941e97f96e6fe718bf08bbc9a26033d7
shansou504 <123512155+shansou504@users.noreply.github.com>
2023-11-17 16:17:09 -0500

    force to top right corner with offset

--- dlogout.c
+++ dlogout.c
@@ -47,12 +47,11 @@ activate (GtkApplication *app,
 	gint h = 80;
 	GtkWidget *window;
 	window = gtk_application_window_new (app);
-//	gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
+	gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
 	gtk_window_set_default_size (GTK_WINDOW (window), w, h);
 	gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
 	gtk_window_set_gravity (GTK_WINDOW (window), GDK_GRAVITY_NORTH_EAST);
-//	gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_MOUSE);
 	g_signal_connect(window, "key_press_event", G_CALLBACK(check_escape), NULL);
 
 	GdkDisplay *display;
@@ -63,20 +62,7 @@ activate (GtkApplication *app,
 	gdk_monitor_get_geometry (monitor, &geometry); 
 	gint a,b,x,y;
 	gtk_window_get_position (GTK_WINDOW (window), &x, &y);
-//	if (x+w > geometry.width - 201) {
-//		a = x+90;
-//	}
-//	else {
-		a = geometry.width - w - 20;
-//	}
-//	if (y+h > geometry.height - 201) {
-//		b = y+100;
-//	}
-//	else {
-//		b = y;
-//	}
-	gtk_window_move (GTK_WINDOW (window), a, 0);
-//	g_print("gemoetry %d,%d\nw,h %d,%d\nx,y %d,%d\na,b %d,%d\n", geometry.width, geometry.height, w, h, x, y, a, b);
+	gtk_window_move (GTK_WINDOW (window), geometry.width - w - 20, 0);
 	
 	GtkWidget *listbox;
 	listbox = gtk_list_box_new ();
@@ -85,14 +71,12 @@ activate (GtkApplication *app,
 	
 	GtkWidget *rowlock = gtk_list_box_row_new();
 	GtkWidget *labellock = gtk_label_new("Lock Screen");
-	//GtkWidget *labellock = gtk_label_new("\uf023\tLock\t");
 	gtk_container_add (GTK_CONTAINER (rowlock), labellock);
 	gtk_container_add (GTK_CONTAINER (listbox), rowlock);
 	gtk_label_set_xalign (GTK_LABEL(labellock), 0);
 	
 	GtkWidget *rowquit = gtk_list_box_row_new();
 	GtkWidget *labelquit = gtk_label_new("Logout");
-	//GtkWidget *labelquit = gtk_label_new("\uf2f5\tQuit\t");
 	gtk_container_add (GTK_CONTAINER (rowquit), labelquit);
 	gtk_container_add (GTK_CONTAINER (listbox), rowquit);
 	gtk_label_set_xalign (GTK_LABEL(labelquit), 0);
@@ -100,13 +84,11 @@ activate (GtkApplication *app,
 	GtkWidget *rowreboot = gtk_list_box_row_new();
 	gtk_container_add (GTK_CONTAINER (listbox), rowreboot);
 	GtkWidget *labelreboot = gtk_label_new("Reboot");
-	//GtkWidget *labelreboot = gtk_label_new("\uf2f1\tReboot\t");
 	gtk_container_add (GTK_CONTAINER (rowreboot), labelreboot);
 	gtk_label_set_xalign (GTK_LABEL(labelreboot), 0);
 	
 	GtkWidget *rowshutdown = gtk_list_box_row_new();
 	GtkWidget *labelshutdown = gtk_label_new("Shutdown");
-	//GtkWidget *labelshutdown = gtk_label_new("\uf011\tShutdown\t");
 	gtk_container_add (GTK_CONTAINER (rowshutdown), labelshutdown);
 	gtk_container_add (GTK_CONTAINER (listbox), rowshutdown);
 	gtk_label_set_xalign (GTK_LABEL(labelshutdown), 0);
